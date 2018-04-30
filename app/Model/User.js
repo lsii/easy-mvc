69a434c62554dca0e689f8da30c33de4d1558185
lsii <ish0.01st@gmail.com>
2018-04-30 18:56:45 +0900

    Add knex, objection, mysql2

--- app/Model/User.js
+++ app/Model/User.js
@@ -1,3 +1,9 @@
-module.exports = {
+const { Model } = require('objection');
 
+class User extends Model {
+  static get tableName() {
+    return 'user';
+  }
 }
+
+module.exports = User;
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'easy_mvc',
+      user:     'root',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
