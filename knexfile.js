6dd89f0313ae1b64a801c54f9e7c3b06f300b731
lsii <ish0.01st@gmail.com>
2018-05-01 01:11:57 +0900

    Mod knexfile

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,11 @@ module.exports = {
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 
